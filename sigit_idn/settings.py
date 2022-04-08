3f1cce3ab889189dcf4b6d106bde6b2910b4a6ea
sigit <sigit.idn@gmail.com>
2022-04-08 12:14:17 +0900

    Set default settings

--- sigit_idn/settings.py
+++ sigit_idn/settings.py
@@ -17,6 +17,8 @@ import re
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sigit_idn.settings')
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
